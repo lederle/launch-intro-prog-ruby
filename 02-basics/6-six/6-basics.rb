5e4ade5f25667cfbfa20bc9f81c37c27ec2a4724
mike lederle <mrlederle@gmail.com>
2019-12-04 15:43:08 -0500

    [2.6] squares of floats

---
+++ 02-basics/6-six/6-basics.rb
@@ -0,0 +1,7 @@
+def square(x)
+  x * x
+end
+
+puts "square of #{3.14} is #{square(3.14)}"
+puts "square of #{6.28} is #{square(6.28)}"
+puts "square of #{438929284837939338943.1} is #{square(438929284837939338943.1)}"
