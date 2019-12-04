4968df29683e858d9a831f080014666002856e8f
mike lederle <mrlederle@gmail.com>
2019-12-04 15:10:51 -0500

    [2.3] hash literal; dereferencing

---
+++ 02-basics/3-three/3-basics.rb
@@ -0,0 +1,13 @@
+movies = {
+  :jaws => '1975', 
+  :eternal_sunshine => '2004', 
+  :gravity => '2013', 
+  :shrek => '2011', 
+  :stripes => '1981'
+}
+
+puts movies[:jaws]
+puts movies[:eternal_sunshine]
+puts movies[:gravity]
+puts movies[:shrek]
+puts movies[:stripes]
