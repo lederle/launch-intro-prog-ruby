8b7dbb1e588a569595c608161fc97878090f139a
mike lederle <mrlederle@gmail.com>
2019-12-06 09:06:56 -0500

    [4.2] expression evaluation

---
+++ 04-methods/2-two/2-methods.rb
@@ -0,0 +1,27 @@
+x = 2
+# assignment evaluates to the RHS, so this expression
+# evaluates to 2
+#
+
+puts x = 2
+# puts always evaluates to nil
+#
+
+p name = "Joe"
+# assignment evaluates to the RHS, so name = "Joe" 
+# evaluates to "Joe", and p evaluates to the object 
+# passed to it, which is "Joe"
+#
+
+four = "four"
+# assignment evaluates to the RHS, so this returns "four"
+#
+
+print something = "nothing"
+# the assignment evaluates to "nothing", and print always 
+# evalutes to nil, so this expresion evaluates to nil
+#
+
+
+
+
