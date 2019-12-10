6f74c0f38fb01336fb49edeac1c9a344fc462cb8
mike lederle <mrlederle@gmail.com>
2019-12-10 14:04:52 -0500

    [5.6] fix if statement error

---
+++ 05-flow_control/6-six/6-flow_control.rb
@@ -0,0 +1,23 @@
+# def equal_to_four(x)
+#   if x == 4
+#     puts "yup"
+#   else
+#     puts "nope"
+# end
+# 
+# equal_to_four(5)
+#
+
+# The code throws an error because the if statement is
+# not closed with an end. Fix it by adding the end.
+#
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
