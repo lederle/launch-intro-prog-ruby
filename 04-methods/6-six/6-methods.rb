fc6ca157657f45b91d3d6cb7d80c653e3fbc127c
mike lederle <mrlederle@gmail.com>
2019-12-10 10:36:34 -0500

    [4.6] Error message analysis

---
+++ 04-methods/6-six/6-methods.rb
@@ -0,0 +1,11 @@
+# ArgumentError: wrong number of arguments (1 for 2)
+#   from (irb):1:in `calculate_product'
+#   from (irb):4
+#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+# Two arguments were required, and instead one was given.
+
+# From docs:
+# "Raised when the arguments are wrong and there isn't a more 
+# specific Exception class."
+# Ref: https://ruby-doc.org/core-2.6.5/ArgumentError.html
