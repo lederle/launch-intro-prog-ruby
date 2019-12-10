90a4be24761cf786a2a115dd36532fac9eba110d
mike lederle <mrlederle@gmail.com>
2019-12-10 10:27:45 -0500

    [4.4] return short-circuit

---
+++ 04-methods/4-four/4-methods.rb
@@ -0,0 +1,11 @@
+def scream(words)
+  words = words + "!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# "Nothing is printed to the screen, the return on line 3 
+# short-circuits appending the exclaimations on line 2."
+
