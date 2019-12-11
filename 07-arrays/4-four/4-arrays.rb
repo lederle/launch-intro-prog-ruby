2c495bd627eef9725738115ed9dd0308948a2232
mike lederle <mrlederle@gmail.com>
2019-12-11 10:05:37 -0500

    [7.4] array index and dereferencing

---
+++ 07-arrays/4-four/4-arrays.rb
@@ -0,0 +1,12 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+p arr.index(5)
+# what is the index of 5 (the first one)
+#
+
+#p arr.index[5]
+# NoMethodError: no method []
+#
+
+p arr[5]
+# dereference the 5th element (zero indexing)
