89b838711cd66c5d03320af9d7b4c1f11d654bd3
mike lederle <mrlederle@gmail.com>
2019-12-11 13:03:26 -0500

    [10-8] hash literal forms

---
+++ 10-EXERCISE/08-eight/8-EX.rb
@@ -0,0 +1,6 @@
+# string as key, old style
+p grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+# symbol as key, old style
+p options = { :font_size => 10, :font_family => "Arial" }
+# symbol as key, new style
+p options = { font_size: 10, font_family: "Arial" }
