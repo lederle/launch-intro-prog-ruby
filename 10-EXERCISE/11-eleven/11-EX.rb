0e22fed59225600bbdd2e7cef1910482e6043b2e
mike lederle <mrlederle@gmail.com>
2019-12-11 13:17:48 -0500

    [10.11] ruby docs

---
+++ 10-EXERCISE/11-eleven/11-EX.rb
@@ -0,0 +1,5 @@
+# At a glance, the rails documentation looks more organized
+# than the ruby.org docs. I have gotten used to the ruby docs
+# from doing these exercises, but it is pretty 2005. The search
+# is terrible.
+#
